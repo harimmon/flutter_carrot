9fbcea14fbec6f04ab87b15e27e6462e5cfefc71
harimmon <bhr0502@gmail.com>
2025-06-02 11:48:54 +0900

    BottomNavigationBar 완성

---
+++ lib/screens/my_carrot/my_carrot_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyCarrotScreen extends StatelessWidget {
+  const MyCarrotScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    print("MyCarrotScreen Build");
+
+    return Scaffold(
+      body: Center(child: Text("MyCarrotScreen")),
+    );
+  }
+}
---
+++ lib/screens/near_me/near_me_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NearMeScreen extends StatelessWidget {
+  const NearMeScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    print("NearMeScreen Build");
+
+    return Scaffold(
+      body: Center(child: Text("NearMeScreen")),
+    );
+  }
+}
